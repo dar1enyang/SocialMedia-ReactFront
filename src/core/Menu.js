90e25549d2ffc619eb93bf7f389b134b0b593230
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-26 20:14:02 -0400

    Users signup and signin

---
+++ src/core/Menu.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { signout, isAuthenticated } from '../auth';
+
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: '#ff9900' };
+  } else {
+    return { color: '#ffffff' };
+  }
+};
+
+const Menu = ({ history }) => (
+  <div>
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, '/')} to="/">
+          Home
+        </Link>
+      </li>
+
+      {!isAuthenticated() && (
+        <>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, '/signin')}
+              to="/signin"
+            >
+              Sign In
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, '/signup')}
+              to="/signup"
+            >
+              Sign Up
+            </Link>
+          </li>
+        </>
+      )}
+
+      {isAuthenticated() && (
+        <>
+          <li className="nav-item">
+            <span
+              className="nav-link"
+              style={
+                (isActive(history, '/signup'),
+                { cursor: 'pointer', color: '#fff' })
+              }
+              onClick={() => signout(() => history.push('/'))}
+            >
+              Sign Out
+            </span>
+          </li>
+
+          <li className="nav-item">
+            <span className="nav-link">{isAuthenticated().user.name}</span>
+          </li>
+        </>
+      )}
+    </ul>
+    ;
+  </div>
+);
+
+export default withRouter(Menu);
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { signup } from '../auth';
+import { Redirect } from 'react-router-dom';
+import { signin, authenticate } from '../auth';
 
-class Signup extends Component {
+class Signin extends Component {
   constructor() {
     super();
     this.state = {
-      name: '',
       email: '',
       password: '',
       error: '',
-      open: false
+      redirectToReferer: false,
+      loading: false
     };
   }
 
@@ -20,37 +21,28 @@ class Signup extends Component {
 
   clickSubmit = event => {
     event.preventDefault();
-    const { name, email, password } = this.state;
+    this.setState({ loading: true });
+    const { email, password } = this.state;
     const user = {
-      name,
       email,
       password
     };
     //console.log(user);
-    signup(user).then(data => {
-      if (data.error) this.setState({ error: data.error });
-      else
-        this.setState({
-          error: '',
-          name: '',
-          email: '',
-          password: '',
-          open: true
+    signin(user).then(data => {
+      if (data.error) {
+        this.setState({ error: data.error, loading: false });
+      } else {
+        // authenticate
+        authenticate(data, () => {
+          this.setState({ redirectToReferer: true });
         });
+        // redirect
+      }
     });
   };
 
-  signupForm = (name, email, password) => (
+  signinForm = (email, password) => (
     <form>
-      <div className="form-group">
-        <label className="text-muted">Name</label>
-        <input
-          onChange={this.handleChange('name')}
-          type="text"
-          className="form-control"
-          value={name}
-        />
-      </div>
       <div className="form-group">
         <label className="text-muted">Email</label>
         <input
@@ -75,10 +67,14 @@ class Signup extends Component {
     </form>
   );
   render() {
-    const { name, email, password, error, open } = this.state;
+    const { email, password, error, redirectToReferer, loading } = this.state;
+
+    if (redirectToReferer) {
+      return <Redirect to="/" />;
+    }
     return (
       <div className="container">
-        <h2 className="mt-5 mb-5">Signup</h2>
+        <h2 className="mt-5 mb-5">SignIn</h2>
 
         <div
           className="alert alert-danger"
@@ -86,18 +82,17 @@ class Signup extends Component {
         >
           {error}
         </div>
-
-        <div
-          className="alert alert-info"
-          style={{ display: open ? '' : 'none' }}
-        >
-          New Account is successfully create. Please Sign in.
-        </div>
-
-        {this.signupForm(name, email, password)}
+        {loading ? (
+          <div className="jumbotron text-center">
+            <h2>Loading...</h2>
+          </div>
+        ) : (
+          ''
+        )}
+        {this.signinForm(email, password)}
       </div>
     );
   }
 }
 
-export default Signup;
+export default Signin;

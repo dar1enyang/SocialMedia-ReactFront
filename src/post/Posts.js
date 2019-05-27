fd67c90f047b9fe8458e3f0730feb224b1b72749
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-05-27 07:25:09 -0400

    Login with Social Network (Login with Google)

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -7,19 +7,34 @@ class Posts extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      posts: [],
+      page: 1
     };
   }
 
-  componentDidMount() {
-    list().then(data => {
+  loadPosts = page => {
+    list(page).then(data => {
       if (data.error) {
         console.log(data.error);
       } else {
         this.setState({ posts: data });
       }
     });
+  };
+
+  componentDidMount() {
+    this.loadPosts(this.state.page);
   }
+
+  loadMore = number => {
+    this.setState({ page: this.state.page + number });
+    this.loadPosts(this.state.page + number);
+  };
+
+  loadLess = number => {
+    this.setState({ page: this.state.page - number });
+    this.loadPosts(this.state.page - number);
+  };
 
   renderPosts = posts => {
     return (
@@ -44,7 +59,7 @@ class Posts extends Component {
                 <p className="card-text">{post.body.substring(0, 100)}</p>
                 <br />
                 <p className="font-italic mark">
-                  Posted bt <Link to={`${posterId}`}>{posterName} </Link>
+                  Posted by <Link to={`${posterId}`}>{posterName} </Link>
                   on {new Date(post.created).toDateString()}
                 </p>
                 <Link
@@ -62,13 +77,36 @@ class Posts extends Component {
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, page } = this.state;
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">
-          {!posts.length ? 'Loading...' : 'Recent Posts'}
+          {!posts.length ? 'No more posts!' : 'Recent Posts'}
         </h2>
+
         {this.renderPosts(posts)}
+
+        {page > 1 ? (
+          <button
+            className="btn btn-raised btn-warning mr-5 mt-5 mb-5"
+            onClick={() => this.loadLess(1)}
+          >
+            Previous ({this.state.page - 1})
+          </button>
+        ) : (
+          ''
+        )}
+
+        {posts.length ? (
+          <button
+            className="btn btn-raised btn-success mt-5 mb-5"
+            onClick={() => this.loadMore(1)}
+          >
+            Next ({page + 1})
+          </button>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
